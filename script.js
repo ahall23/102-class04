788584e17325b337f2f4bf0e3bb06674285805c0
Alexia Hall <alexiahall306@icloud.com>
2023-02-13 14:48:20 -0600

    07lab

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function apply() {
-  let name = prompt("What is your name?");
-  let age = prompt("how old are you?");
+  let name = prompt("What is your name Beezuiful?");
+  let age = parsentInt(prompt("How old are you?"));
 
+  if (isNaN(age)) {
+    alert("EORROR please try again🫤");
+  }
   if (age > 18) {
-    alert("😎 Cool Beez!" + " " + name + " " + "Come Save some Beez!");
+    alert("😎 Cool Beez!" + " " + name + " " + "Come Save some Beez!🐝");
   } else {
-    alert("🥹" + " " + name + " " + "Maybe next time");
+    alert("🥹" + " " + name + " " + "EORROR Maybe next time");
   }
 }
-
 let applyButton = document.querySelector("button");
 applyButton.addEventListener("click", apply);
